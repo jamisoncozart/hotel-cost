824605bf2320aff6512e57cf51b293450cc3e11f
Jamison Cozart <jamisoncozart@gmail.com>
2020-02-21 15:17:12 -0800

    Take console user input and store values in variables, define booleans based on user input

--- HotelCost.cs
+++ HotelCost.cs
@@ -4,6 +4,13 @@ class HotelCost
 {
   static void Main()
   {
-    //code
+    Console.WriteLine("What season will you be booking your stay with us? (winter, spring, summer, fall)");
+    string season = Console.ReadLine();
+    
+    Console.WriteLine("Will you be booking on a weekend, or a weekday?");
+    string dayOfWeek = Console.ReadLine();
+
+    bool summer = season == "summer";
+    bool weekend = dayOfWeek == "weekend";
   }
 }
